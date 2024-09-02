dfbb098e5a5fbe15ba19a0ad2c456067f39d5837
juanMotok32 <juan.motok@gmail.com>
2024-09-01 21:41:07 -0300

    Cree la screen de bienvenida

---
+++ screens/Bienvenido.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import './css/Bienvenido.css';
+import logoBlack from './imagenes/BrisaBlack.png';
+
+function Bienvenido() {
+  return (
+    <div className='container'>
+      <div>
+        <img src={logoBlack} alt="Logo en modo oscuro" className="logo" />
+      </div>
+      <h1 className="Bienvenido">
+        <span className="white"> BR</span>
+        <span className="desColor">I</span>
+        <span className="white">S</span>
+        <span className="desColor">A</span>
+      </h1>
+      <h3 className="Descripcion">
+        <span className='desColor'>Objetivo del Proyecto </span>: Desarrollar una inteligencia artificial capaz de identificar patrones específicos
+        a travez de una analisis con IA, que utiliza imagenes de la retina para detectar enfermedades, afecciones o patologías en
+        los vasos sanguíneos de la retina.  El propósito es asistir a los profesionales de la salud en la detección temprana de anomalías,
+        facilitando así el diagnóstico y mejorando la calidad del cuidado ocular.
+      </h3>
+      <h4 className='Objetivo'>Para cumplir con nuestro objetivo, usaremos las imágenes obtenidas por los siguientes estudios oculares
+      </h4>
+      <h3 className='titulosEstudios'>FONDO DE OJOS</h3>
+      <h4 className='DescFondo'>examen que permite al oftalmólogo observar el interior del ojo, incluyendo la retina, el nervio óptico, los vasos sanguíneos y otras estructuras
+      </h4>
+      <h3 className='titulosEstudios'>FLUORESCEINOGRAMA (ANGIOGRAFÍA CON FLUORESCEÍNA)      </h3>
+      <h4 className='DescFondo'>estudio que evalúa la circulación de la sangre en la retina. Se inyecta un colorante fluorescente para poder detectar bloqueos en los vasos sanguíneos, o anormalidades en la estructura de la retina.
+      </h4>
+    </div>
+  );
+}
+
+export default Bienvenido;

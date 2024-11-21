37e32cbf9bc5cfda6f358526e6d657d94b8afed0
francodiiorio <franco.diiorio5@gmail.com>
2024-11-21 00:12:50 -0300

    integracion de backend

--- src/auth/pages/Auth/Auth.jsx
+++ src/auth/pages/Auth/Auth.jsx
@@ -5,12 +5,18 @@ import Card from '../../../shared/components/UIElements/Card'
 import Input from '../../../shared/components/FormElements/Input/Input'
 import { VALIDATOR_EMAIL, VALIDATOR_MINLENGTH, VALIDATOR_REQUIRE } from '../../../shared/util/validators'
 import { useForm } from "../../../shared/hooks/form-hook";
+import { useHttpClient } from "../../../Shared/hooks/http-hook";
 import Button from "../../../Shared/components/FormElements/Button";
 import { AuthContext } from "../../../Shared/context/auth-context";
+import ErrorModal from "../../../Shared/components/UIElements/ErrorModal/ErrorModal";
+import LoadingSpinner from "../../../Shared/components/UIElements/LoadingSpinner/LoadingSpinner";
 
 const Auth = () => {
   const auth = useContext(AuthContext)
   const [isLogin, setIsLogin] = useState(true)
+  const [isLoading, setIsLoading] = useState(false)
+  const [error, setError] = useState()
+
   const [formState, inputHandler, setFormData] = useForm({
     email: {
       value: '',
@@ -40,14 +46,71 @@ const Auth = () => {
     setIsLogin(prevMode => !prevMode);
   }
 
-  const authSubmitHandler = event => {
+  const authSubmitHandler = async event => {
     event.preventDefault();
-    console.log(formState.inputs);
-    auth.login();
+    setIsLoading(true)
+
+    if(isLogin) {
+      try {
+        const response = await fetch('http://localhost:3000/login/', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            email: formState.inputs.email.value,
+            password: formState.inputs.password.value
+          })
+        });
+
+        const responseData = await response.json()
+        if (!response.ok){
+          throw new Error(responseData.message)
+        }
+        setIsLoading(false)
+        auth.login();
+      } catch (err) {
+        console.log(err)
+        setIsLoading(false)
+        setError(err.message || 'Algo salio mal, por favor intentalo de nuevo.')
+      }
+    } else {
+      try {
+        const response = await fetch('http://localhost:3000/register/', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            username: "prueba",
+            email: formState.inputs.email.value,
+            password: formState.inputs.password.value
+          })
+        });
+
+        const responseData = await response.json()
+        if (!response.ok){
+          throw new Error(responseData.message)
+        }
+        setIsLoading(false)
+        auth.login();
+      } catch (err) {
+        console.log(err)
+        setIsLoading(false)
+        setError(err.message || 'Algo salio mal, por favor intentalo de nuevo.')
+      }
+    }
   }
+
+  const errorHandler = () => {
+    setError(null)
+  }
+
   return (
     <div className={styles.container}>
+      <ErrorModal error={error} onClear={errorHandler}/>
     <Card className={styles.authentication}>
+      {isLoading && <LoadingSpinner asOverlay/>}
       <h2 className={styles.card__title}>Iniciar Sesión</h2>
       <form onSubmit={authSubmitHandler}>
         {!isLogin && (
